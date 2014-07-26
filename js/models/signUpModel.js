05a46ef48c9c4f75744ab354d72ae88b423a4802
viiqswim <v1ct0rs3xy@hotmail.com>
2014-07-26 15:35:10 -0600

    signup now talks to server

---
+++ js/models/signUpModel.js
@@ -0,0 +1,27 @@
+SignUpModel = Backbone.Model.extend({
+    defaults: {
+        status: '',
+    },
+    initialize: function(){
+    },
+    registerUser: function(args){
+        var _self = this;
+        $.ajax({
+            type: 'POST',
+            url: 'registerUser',
+            data: {
+                username: args.username,
+                password: args.password,
+                email: args.email,
+                phone: args.phone,
+                firstname: args.firstname,
+                lastname: args.lastname,
+                avatar_url: args.avatar_url,
+                job: args.job,
+            },
+            success: function(data){
+                _self.set('status', jQuery.parseJSON(data));
+            }
+        });
+    },
+});
